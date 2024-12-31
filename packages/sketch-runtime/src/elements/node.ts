8dd63d2e296ca9be2ae9b43f440fba13347b4b71
yu.pan <panyupy@vip.qq.com>
2024-12-31 21:56:36 +0800

    feat: 串联节点链路

--- packages/sketch-runtime/src/elements/node.ts
+++ packages/sketch-runtime/src/elements/node.ts
@@ -1,6 +1,18 @@
+import {SketchRoot} from "./root";
+
 export class SketchNode {
 
     public parentNode: SketchNode | null = null
+
     public childNodes: SketchNode[] = []
+
+    public get _root (): SketchRoot | null {
+        return this.parentNode?._root || null
+    }
+
+    public appendChild (newChild: SketchNode) {
+        newChild.parentNode = this
+        this.childNodes.push(newChild)
+    }
 }
 
--- packages/sketch-runtime/src/elements/root.ts
+++ packages/sketch-runtime/src/elements/root.ts
@@ -1,4 +1,8 @@
 import {SketchElement} from "./element";
 
 export class SketchRoot extends SketchElement {
+
+    public get _root (): SketchRoot {
+        return this
+    }
 }
