1d526d8b05d55f0dacc9d04375b644855199dc07
yu.pan <panyupy@vip.qq.com>
2025-01-02 00:05:00 +0800

    feat: 调整优化

--- packages/sketch-runtime/src/elements/node.ts
+++ packages/sketch-runtime/src/elements/node.ts
@@ -5,32 +5,32 @@ import { SketchRoot } from './root'
  */
 export abstract class SketchNode {
   /**
+   * 渲染函数抽象
+   */
+  public abstract render(): Promise<void>
+
+  /**
+   * 子节点
+   */
+  public childNodes: SketchNode[] = []
+
+  /**
      * 父节点
      */
   public parentNode: SketchNode | null = null
 
   /**
-     * 子节点
-     */
-  public childNodes: SketchNode[] = []
-
-    /**
-     * 渲染函数抽象
-     */
-    public abstract render(): Promise<void>
-
-    /**
      * 获取根节点
      */
-    public get _root (): SketchRoot | null {
-      return this.parentNode?._root || null
-    }
+  public get _root (): SketchRoot | null {
+    return this.parentNode?._root || null
+  }
 
-    /**
+  /**
      * 插入根节点
      */
-    public appendChild (newChild: SketchNode) {
-      newChild.parentNode = this
-      this.childNodes.push(newChild)
-    }
+  public appendChild (newChild: SketchNode) {
+    newChild.parentNode = this
+    this.childNodes.push(newChild)
+  }
 }
