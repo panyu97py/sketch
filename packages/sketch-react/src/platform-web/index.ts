b80c3927230a3bee6adede024d9e3e23e827285e
yu.pan <panyupy@vip.qq.com>
2025-01-03 23:44:48 +0800

    feat: 增加 runtime 导出

--- packages/sketch-react/src/platform-web/index.ts
+++ packages/sketch-react/src/platform-web/index.ts
@@ -3,6 +3,8 @@ import { InternalSketchView } from '../common/view'
 import { InternalSketchText } from '../common/text'
 import { InternalSketchWebImage } from './components/image'
 
+export * from '@sketchjs/runtime'
+
 export const Sketch = Object.assign({}, {
   Root: InternalSketchRoot,
   View: InternalSketchView,
