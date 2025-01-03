faabcf5c0348bd56835e4070bfb3bcf54192f032
yu.pan <panyupy@vip.qq.com>
2025-01-03 18:46:48 +0800

    feat: 胶水层初始化

--- packages/sketch-react/src/index.ts
+++ packages/sketch-react/src/index.ts
@@ -1,7 +1,7 @@
 if (process.env.SKETCH_PLATFORM === 'APPLET') {
-  module.exports = require('./applet')
+  module.exports = require('./platform-applet')
 } else if (process.env.SKETCH_PLATFORM === 'H5') {
-  module.exports = require('./h5')
+  module.exports = require('./platform-web')
 } else {
   console.warn('No matching platform')
 }
