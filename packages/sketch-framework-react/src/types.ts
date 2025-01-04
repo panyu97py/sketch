9017d305cce83a29a7f8bc04865448d6ec178c8c
yu.pan <panyupy@vip.qq.com>
2025-01-05 04:26:58 +0800

    feat: 增加类型文件按环境变量导出

---
+++ packages/sketch-framework-react/src/types.ts
@@ -0,0 +1,18 @@
+import React from 'react'
+import { SketchElementChild, SketchElementProps, SketchHandler } from './common/types'
+import { InternalSketchRootProps, InternalSketchViewProps, InternalSketchTextProps } from './common/components'
+
+export * from '@sketchjs/runtime'
+export * from './common/types'
+
+interface InternalSketchImageProps extends SketchElementProps{
+    src?: string;
+    children?: SketchElementChild|SketchElementChild[];
+}
+
+export declare const Sketch: {
+    Root: React.ForwardRefExoticComponent<InternalSketchRootProps & React.RefAttributes<SketchHandler>>;
+    View: React.FC<InternalSketchViewProps>;
+    Text:React.FC<InternalSketchTextProps>;
+    Image: React.FC<InternalSketchImageProps>;
+}
