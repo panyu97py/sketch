153b99675893b3d5a041e019846379af84490b95
yu.pan <panyupy@vip.qq.com>
2025-04-04 20:04:41 +0800

    feat: vue demo 调整

--- example/example-h5-vue/src/App.vue
+++ example/example-h5-vue/src/App.vue
@@ -1,85 +1,70 @@
-<script setup lang="ts">
-import { RouterLink, RouterView } from 'vue-router'
-import HelloWorld from './components/HelloWorld.vue'
-</script>
-
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-
-      <nav>
-        <RouterLink to="/">Home</RouterLink>
-        <RouterLink to="/about">About</RouterLink>
-      </nav>
-    </div>
-  </header>
-
-  <RouterView />
+  <div class="App" @click="handleToDataURL">
+    <canvas class="sketch-canvas" ref="canvas"/>
+    <Sketch.Root :style="style.root" :sketch="sketch" @ready="handleSketchInitialized" @update="handleSketchUpdate">
+      <Sketch.View :style="style.view">
+        <Sketch.Image src="@/assets/logo.svg" :style="style.logo"/>
+        <Sketch.Text text="Hello  World!" :style="style.text"/>
+      </Sketch.View>
+    </Sketch.Root>
+  </div>
 </template>
 
+<script setup lang="ts">
+
+import { StyleSheet, Sketch } from '@sketchjs/vue'
+
+Sketch.debug = true
+
+const style = StyleSheet.create({
+root: {
+  width: 500,
+  height: 500,
+  backgroundColor: '#282c34'
+},
+view: {
+  width: 500,
+  height: 500,
+  justifyContent: 'center',
+  alignItems: 'center'
+},
+logo: {
+  width: 200,
+  height: 200
+},
+text: {
+  width: 500,
+  marginTop: 20,
+  color: '#ffffff',
+  fontSize: 50,
+  fontWeight: 400,
+  lineHeight: 50,
+  textAlign: 'center'
+}
+})
+
+const sketch = Sketch.useSketch()
+
+const handleToDataURL = () => {
+  const dataUrl = sketch.toDataURL('image/png', 1)
+  console.log({ dataUrl })
+}
+
+const handleSketchUpdate = () => {
+  console.log('sketch update')
+}
+
+const handleSketchInitialized = () => {
+  console.log('sketch initialized')
+}
+</script>
+
 <style scoped>
-header {
-  line-height: 1.5;
-  max-height: 100vh;
-}
-
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-nav {
-  width: 100%;
-  font-size: 12px;
-  text-align: center;
-  margin-top: 2rem;
-}
-
-nav a.router-link-exact-active {
-  color: var(--color-text);
-}
-
-nav a.router-link-exact-active:hover {
-  background-color: transparent;
-}
-
-nav a {
-  display: inline-block;
-  padding: 0 1rem;
-  border-left: 1px solid var(--color-border);
-}
-
-nav a:first-of-type {
-  border: 0;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-
-  nav {
-    text-align: left;
-    margin-left: -1rem;
-    font-size: 1rem;
-
-    padding: 1rem 0;
-    margin-top: 1rem;
-  }
+.App {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 100vh;
+  width: 100vw;
 }
 </style>
