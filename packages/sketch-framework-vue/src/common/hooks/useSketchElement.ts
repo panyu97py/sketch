ec99990598ec1ac7820d50599a2b66e240d41e0d
yu.pan <panyupy@vip.qq.com>
2025-09-09 18:19:07 +0800

    feat: 生成 VNode hash 作为元素默认 key

--- packages/sketch-framework-vue/src/common/hooks/useSketchElement.ts
+++ packages/sketch-framework-vue/src/common/hooks/useSketchElement.ts
@@ -1,6 +1,21 @@
 import { SketchElement } from '@sketchjs/runtime'
+import objectHash from 'object-hash'
 import { getCurrentInstance, inject, provide, useSlots, watchEffect, VNode } from 'vue'
 import { noop } from 'lodash-es'
+
+const generateKeyFromVNode = (vNode: VNode): PropertyKey => {
+  if (vNode.key) return vNode.key
+
+  const typeStr = (() => {
+    if (typeof vNode.type === 'string') return vNode.type
+    if ((vNode.type as any)?.name) return (vNode.type as any)?.name
+    return 'component'
+  })()
+
+  const propsHash = (objectHash(vNode.props) as string).slice(0, 8)
+
+  return `${typeStr}-${propsHash}`
+}
 
 type Nullable<T> = T | undefined;
 
@@ -10,13 +25,13 @@ interface Opts {
 
 export const useSketchElement = (opt: Opts) => {
 
-  const { self} = opt
+  const { self } = opt
 
   const slots = useSlots()
 
   const instance = getCurrentInstance()
 
-  const sketchElementMap  = new Map<VNode,SketchElement>()
+  const sketchElementMap = new Map<PropertyKey, SketchElement>()
 
   const parent = inject<SketchElement>('parent')
 
@@ -28,24 +43,29 @@ export const useSketchElement = (opt: Opts) => {
 
   provide('registerChild', (element: SketchElement, vNode: VNode) => {
 
+    const vNodeKey = generateKeyFromVNode(vNode)
+
     const childrenVNodes = slots.default?.() || []
 
-    // TODO 这里的对比方法有问题
-    const childIndex = childrenVNodes.indexOf(vNode)
+    const childrenVNodeKeys = childrenVNodes.map(item => generateKeyFromVNode(item))
+
+    if (Array.from(new Set(childrenVNodeKeys)).length !== childrenVNodes.length) throw new Error('存在相同的 key 或 VNode 需要手动指定不会重复的元素 key')
+
+    const childIndex = childrenVNodeKeys.findIndex((item) => vNodeKey == item)
 
     const nextRegisteredChild = childrenVNodes.find((item, index) => {
-      return index > childIndex && sketchElementMap.has(item)
+      return index > childIndex && sketchElementMap.has(generateKeyFromVNode(item))
     })
 
-    const nextSibling = nextRegisteredChild ? sketchElementMap.get(nextRegisteredChild) : undefined
+    const nextSibling = nextRegisteredChild ? sketchElementMap.get(generateKeyFromVNode(nextRegisteredChild)) : undefined
 
-    sketchElementMap.set(vNode, element)
+    sketchElementMap.set(vNodeKey, element)
 
     self?.insertBefore(element, nextSibling)
   })
 
   provide('unregisterChild', (element: SketchElement, vNode: VNode) => {
-    sketchElementMap.delete(vNode)
+    sketchElementMap.delete(generateKeyFromVNode(vNode))
     self?.removeChild(element)
   })
 
